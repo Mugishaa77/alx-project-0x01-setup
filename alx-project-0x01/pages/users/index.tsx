a6618d7e67e9f973fc6e5c67713c0c6f1a9a482b
Mugishaa77 <Sallywanga2016@gmail.com>
2025-07-20 18:10:25 +0300

    Fixed failing Checks

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,30 @@
+import React from "react";
+import { UserProps } from "../../interfaces";
+import UserCard from "../../components/common/UserCard";
+
+interface UsersProps {
+  users: UserProps[];
+}
+
+const Users: React.FC<UsersProps> = ({ users }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default Users;
+
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      users,
+    },
+  };
 }
